860ebd0e01a1443fd5c6a537285d403fb5d952c9
Brenden Perry <brendenperry5@gmail.com>
2024-10-07 14:14:24 -0700

    Updates to time series objects

--- CustomObjects/TimeSeries.py
+++ CustomObjects/TimeSeries.py
@@ -4,15 +4,11 @@ from CustomObjects.Enums.TimeSeriesPurpose import TimeSeriesPurpose
 class TimeSeries:
     _time_series_purpose = TimeSeriesPurpose.Unclassified
     _time_series_title = ''
-    _x_axis_label = ''
-    _y_axis_label = ''
     _data = {}
 
-    def __init__(self, data: dict,  time_series_title: str, x_axis_label='Date', y_axis_label='Price', time_series_purpose=TimeSeriesPurpose.Unclassified):
+    def __init__(self, data: dict[float],  time_series_title: str, time_series_purpose=TimeSeriesPurpose.Unclassified):
         self._data = data
         self._time_series_title = time_series_title
-        self._x_axis_label = x_axis_label
-        self._y_axis_label = y_axis_label
         self._time_series_purpose = time_series_purpose
 
     def get_time_series_purpose(self) -> TimeSeriesPurpose:
@@ -27,8 +23,3 @@ class TimeSeries:
     def get_series_values(self):
         return self._data.values()
 
-    def get_x_axis_label(self):
-        return self._x_axis_label
-
-    def get_x_axis_label(self):
-        return self._y_axis_label
--- CustomObjects/TimeSeriesGroup.py
+++ CustomObjects/TimeSeriesGroup.py
@@ -3,9 +3,12 @@ from CustomObjects.TimeSeries import TimeSeries
 
 class TimeSeriesGroup:
     _data = []
+    _x_axis_label = ''
+    _y_axis_label = ''
 
-    def __int__(self):
-        pass
+    def __init__(self, x_axis_label='Date', y_axis_label='Price'):
+        self._x_axis_label = x_axis_label
+        self._y_axis_label = y_axis_label
 
     def add_series(self, series: TimeSeries):
         self._data.append(series)
@@ -13,5 +16,11 @@ class TimeSeriesGroup:
     def remove_series_by_name(self, time_series_name: str):
         self._data = [x for x in self._data if x.get_time_series_name() != time_series_name]
 
-    def get_all_series(self):
+    def get_all_series(self) -> [TimeSeries]:
         return self._data
+
+    def get_x_axis_label(self) -> str:
+        return self._x_axis_label
+
+    def get_y_axis_label(self) -> str:
+        return self._y_axis_label
